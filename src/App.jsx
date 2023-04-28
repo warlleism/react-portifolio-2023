21b5b03521d931744f4013be57c1e8096f732ba6
Warllei Martins <warllei.s.martins@hotmail.com>
2023-04-28 10:29:39 -0300

    correções

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './style.css'
 import Rotas from './routes/route'
+import { useEffect } from 'react'
 
 function App() {
 
+  useEffect(() => {
+    const spinner = document.getElementById('spinner')
+    setTimeout(() => {
+      spinner.style.display = 'none'
+    }, 4000)
+  }, [])
+
   return (
     <div className="App">
+      <div className='spinner' id='spinner'>
+        <div className="lds-facebook" ><div></div><div></div><div></div></div>
+      </div>
       <Rotas />
     </div>
   )
